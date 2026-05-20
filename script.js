71a64be8936f7f0221298a0fabd4d03697160cae
Bryanspro <bryansx@gmail.com>
2026-05-20 00:26:54 -0400

    increased player size more

--- script.js
+++ script.js
@@ -437,13 +437,13 @@ document.addEventListener('DOMContentLoaded', () => {
                     playerBody.style.setProperty('--player-aspect', '9 / 16');
                     if (modal) {
                         modal.classList.add('vertical-mode');
-                        modal.style.width = window.innerWidth > 600 ? '252px' : '';
+                        modal.style.width = window.innerWidth > 600 ? '264px' : '';
                     }
                 } else {
                     playerBody.style.setProperty('--player-aspect', '16 / 9');
                     if (modal) {
                         modal.classList.remove('vertical-mode');
-                        modal.style.width = window.innerWidth > 600 ? '336px' : '';
+                        modal.style.width = window.innerWidth > 600 ? '352px' : '';
                     }
                 }
             }
